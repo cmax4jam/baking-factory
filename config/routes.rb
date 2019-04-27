802b62e57807f532bffb0f18db58238fa1b4f43f
CJ May <christopher.may706@gmail.com>
2019-04-27 17:45:48 -0400

    very basic login/logout implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   resources :addresses
   resources :customers
   resources :orders
+  resources :users
+  resources :sessions
 
   # Semi-static page routes
   get 'home' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+  get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
   
   # Set the root url
   root :to => 'home#home'
