5f54b925a92037affdc9b2a143cc8258e9fe0413
CJ May <christopher.may706@gmail.com>
2019-05-05 18:03:26 -0400

    better guest/customer index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,16 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         if current_user.role?(:admin)
-          redirect_back(fallback_location: admin_insights_path, notice: "Logged in!")
+          redirect_to admin_insights_path, notice: "Logged in!"
         elsif current_user.role?(:customer)
           create_cart
-          redirect_back(fallback_location: customer_home_path, notice: "Logged in!")
+          redirect_to customer_home_path, notice: "Logged in!"
+        elsif current_user.role?(:baker)
+          redirect_to baker_home_path, notice: "Logged in!"
+        elsif current_user.role?(:shipper)
+          redirect_to shipper_home_path, notice: "Logged in!"
         else
-          redirect_back(fallback_location: home_path, notice: "Logged in!")
+          redirect_to home_path, notice: "Logged in!"
         end
       else
         flash.now.alert = "Email or password is invalid"
